da7eb367572ff99959279a7732b7536587e85972
alejoalzate <alejoalzate12@gmail.com>
2021-01-24 21:14:26 -0500

    Metodo en el aplicationController para capturar mas datos para registrar un usuario

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,15 @@
 class ApplicationController < ActionController::Base
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
     # Metodo que redirecciona al index de cancan
     rescue_from CanCan::AccessDenied do |exception|
         redirect_to root_path, :alert => exception.message
     end
+
+    # Metodo que ayuda a guardar los parametros de el sign_up
+    protected
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation, :user_role) }
+    end
+
 end
